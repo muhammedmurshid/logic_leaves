5dda7853110483c8efcb4fa5810b172f565a42ba
Murshid <muhsink00000@gmail.com>
2023-07-05 11:02:14 +0530

    filter approved

--- models/leaves_reporting.py
+++ models/leaves_reporting.py
@@ -4,11 +4,11 @@ from odoo import api, fields, models, _
 class LeavesReportingLogic(models.Model):
     _inherit = 'hr.leave.report'
 
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        # Add your default filter conditions
-        args += [('state', '=', 'validate')]
-        return super(LeavesReportingLogic, self).search(args, offset, limit, order, count)
+    # @api.model
+    # def search(self, args, offset=0, limit=None, order=None, count=False):
+    #     # Add your default filter conditions
+    #     args += [('state', '=', 'validate')]
+    #     return super(LeavesReportingLogic, self).search(args, offset, limit, order, count)
 
 
 
