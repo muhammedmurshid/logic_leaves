e545a2641cf472209788bc7c3f05958bf30866bf
Murshid <muhsink00000@gmail.com>
2023-11-01 12:31:11 +0530

    joining date domain removed

--- models/casual_leave_allocation.py
+++ models/casual_leave_allocation.py
@@ -5,16 +5,4 @@ class CasualLeaveAllocation(models.Model):
     _inherit = 'hr.employee'
 
     joining_date_cus = fields.Date(string='Date Of Joining')
-
-    @api.onchange('joining_date_cus')
-    def joining_date_compute(self):
-        allocation = self.env['hr.contract'].search([])
-
-        self.joining_date_cus = self.joining_date
-
-        print('hi', self.joining_date_cus)
-        # allocation = self.env['hr.leave.allocation'].create({
-        #     'employee_id': self.id
-        # })
-        # res = super(CasualLeaveAllocation, self).create()
-        # return res
+    
