311a8e763a8fc6f8a0462b884640747d429e5ae0
Murshid <muhsink00000@gmail.com>
2024-06-13 11:26:01 +0530

    add super approval in head approval state

--- models/leave_type.py
+++ models/leave_type.py
@@ -10,9 +10,3 @@ class AddNewLeaveType(models.Model):
         ('manager', "By Employee's Manager"),
         ('both', "By Employee's Manager and Time Off Officer"),
         ('order_manager_to_hr', "Order Manager To HR")], default='hr', string='Leave Validation')
-    # @api.onchange('leave_validation_type')
-    # def onchange_leave_validation_type(self):
-    #     if self.leave_validation_type == 'order_manger_to_hr':
-    #         return {
-    #             'req'
-    #         }
